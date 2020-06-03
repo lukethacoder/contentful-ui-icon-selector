ecba0288bef117d19840daa5b59e908eae541004
Luke Secomb <luke.secomb@live.com>
2020-06-04 00:14:06 +1000

    🔥 base component structure / add some example svg icons (as react components)

---
+++ src/icons/index.tsx
@@ -0,0 +1,5 @@
+export { Caravan } from './caravan';
+export { FighterJet } from './fighter-jet';
+export { Pram } from './pram';
+export { ShoppingCart } from './shopping-cart';
+export { Truck } from './truck';
--- src/index.tsx
+++ src/index.tsx
@@ -1,78 +1,132 @@
-import React, { Fragment, useEffect, useState, FC } from 'react';
+import * as React from 'react';
 import { render } from 'react-dom';
-import { TextInput } from '@contentful/forma-36-react-components';
+import {
+  TextInput,
+  Dropdown,
+  Button,
+  DropdownList,
+  DropdownListItem
+} from '@contentful/forma-36-react-components';
 import { init, FieldExtensionSDK } from 'contentful-ui-extensions-sdk';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import './index.css';
 
-// export class App extends React.Component<AppProps, AppState> {
-//   constructor(props: AppProps) {
-//     super(props);
-//     this.state = {
-//       value: props.sdk.field.getValue() || ''
-//     };
-//   }
+import { Caravan, FighterJet, Pram, ShoppingCart, Truck } from './icons';
 
-//   detachExternalChangeHandler: Function | null = null;
+const ICONS: IconItem[] = [
+  {
+    title: 'Caravan',
+    id: 'CARAVAN',
+    svg: Caravan
+  },
+  {
+    title: 'Fighter Jet',
+    id: 'FIGHTER_JET',
+    svg: FighterJet
+  }
+];
 
-//   componentDidMount() {
-//     this.props.sdk.window.startAutoResizer();
-
-//     // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
-//     this.detachExternalChangeHandler = this.props.sdk.field.onValueChanged(this.onExternalChange);
-//   }
-
-//   componentWillUnmount() {
-//     if (this.detachExternalChangeHandler) {
-//       this.detachExternalChangeHandler();
-//     }
-//   }
-
-//   onExternalChange = (value: string) => {
-//     this.setState({ value });
-//   };
-
-//   onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-//     const value = e.currentTarget.value;
-//     this.setState({ value });
-//     if (value) {
-//       await this.props.sdk.field.setValue(value);
-//     } else {
-//       await this.props.sdk.field.removeValue();
-//     }
-//   };
-
-//   render = () => {
-//     return (
-//       <TextInput
-//         width="large"
-//         type="text"
-//         id="my-field"
-//         testId="my-field"
-//         value={this.state.value}
-//         onChange={this.onChange}
-//       />
-//     );
-//   };
-// }
-
-const App: FC<AppMain> = props => {
+const App: React.FC<AppMain> = props => {
   console.log('props ', props);
-  const [value, setValue] = useState(props.sdk.field.getValue());
 
-  function onChange() {
-    console.log('onChange');
+  const [value_number_one, setValueNumberOne] = React.useState('');
+  const [value_number_two, setValueNumberTwo] = React.useState('');
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  let detachExternalChangeHandler: Function | null = null;
+
+  function onExternalChange(value: any) {
+    if (value) {
+      setValueNumberOne(value.value);
+      setValueNumberTwo(value.value_two);
+    }
+  }
+
+  React.useEffect(() => {
+    props.sdk.window.startAutoResizer();
+    let initial_value = props.sdk.field.getValue();
+
+    if (initial_value) {
+      setValueNumberOne(initial_value.value);
+      setValueNumberTwo(initial_value.value_two);
+    }
+
+    // Handler for external field value changes (e.g. when multiple authors are working on the same entry).
+    if (detachExternalChangeHandler === null) {
+      detachExternalChangeHandler = props.sdk.field.onValueChanged(onExternalChange);
+    }
+  }, []);
+
+  async function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const current_value = e.currentTarget.value;
+    setValueNumberOne(current_value);
+
+    if (current_value) {
+      await props.sdk.field.setValue({ value: current_value, value_two: value_number_two });
+    } else {
+      await props.sdk.field.removeValue();
+    }
+  }
+
+  async function onChangeTwo(e: React.ChangeEvent<HTMLInputElement>) {
+    const current_value = e.currentTarget.value;
+    setValueNumberTwo(current_value);
+
+    if (current_value) {
+      await props.sdk.field.setValue({ value: value_number_one, value_two: current_value });
+    } else {
+      await props.sdk.field.removeValue();
+    }
   }
 
   return (
-    <TextInput
-      width="large"
-      type="text"
-      id="my-field"
-      testId="my-field"
-      value={value}
-      onChange={onChange}
-    />
+    <React.Fragment>
+      <TextInput
+        width="large"
+        type="text"
+        id="value-juan"
+        value={value_number_one}
+        onChange={onChange}
+      />
+      <TextInput
+        width="large"
+        type="text"
+        id="value-two"
+        value={value_number_two}
+        onChange={onChangeTwo}
+      />
+
+      <Dropdown
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        key={Date.now()} // Force Reinit
+        style={{ minHeight: '128px' }}
+        thumbnailElement={
+          <div
+            style={{
+              height: '100%',
+              width: '100%'
+            }}
+          />
+        }
+        toggleElement={
+          <Button
+            size="medium"
+            buttonType="muted"
+            indicateDropdown
+            onClick={() => setIsOpen(!isOpen)}>
+            Choose Icon
+          </Button>
+        }>
+        <DropdownList maxHeight={175}>
+          {ICONS.map(icon => (
+            <DropdownListItem onClick={e => console.log(e.currentTarget.value)}>
+              {icon.title}
+            </DropdownListItem>
+          ))}
+        </DropdownList>
+      </Dropdown>
+    </React.Fragment>
   );
 };
 
@@ -93,10 +147,8 @@ interface AppMain {
   value?: string;
 }
 
-interface AppProps {
-  sdk: FieldExtensionSDK;
-}
-
-interface AppState {
-  value?: string;
+interface IconItem {
+  title: string;
+  id: string;
+  svg: React.FC;
 }
